ac44e4022240e82acf21f9147de6e7aef2a823c3
papapepekk <77702436+papapepekk@users.noreply.github.com>
2021-02-05 01:02:16 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,191 +1,138 @@
 
+
 const help = (prefix) => { 
 	return `                 
 <══════════════════════>
-            CREITIM - BOT
- <══════════════════════>
+            CREITIM - BOT <══════════════════════>
 
-__█████████  ● ᏴϴͲ●ᎷᎬΝႮ●         
+__█████████         
 __█▄█████▄█   Author:ELɢᴜsᴛᴀ
 __█▼▼▼▼▼█ 
-_██ᏴϴͲ Ꮩ4.0██▌ 
+_██CREITIM██▌ 
 __█▲▲▲▲▲█ 
 __█████████ 
 ____██_____██
 
-┌─────────4.0────────┐
-└─────────4.0────────┘
+            M E N U  B O T
 ╔══════════════════════
-║ ᏟϴᎷᎪΝᎠϴՏ ᏞᏆՏͲᎪᎠϴՏ
+║       LISTA DE COMANDOS
 ╠══════════════════════
-║   ᎬХᏟᏞႮՏᏆᏙϴ ᏢᎪᎡᎪ ᎪᎠᎷ'Տ
+║   COMANDOS ᏢᎪᎡᎪ ᎪᎠᎷ'Տ
 ║╭─────────────────────
 ║├> ${prefix}welcome [0/1]
 ║├> ${prefix}bv [0/1]
 ║├> ${prefix}bemvindo [0/1]
 ║├> ${prefix}boasvindas [0/1]
-║├────> desativar/ativa bv
-║├─────────────────────
 ║├> ${prefix}add 55xxx
 ║├> ${prefix}adicionar 55xxx
-║├────> add menbro
-║├─────────────────────
 ║├> ${prefix}kick @membro
 ║├> ${prefix}ban @membro
 ║├> ${prefix}remover @membro
-║├────> remover membro
-║├─────────────────────
-║├> ${prefix}promote @membro
 ║├> ${prefix}promover @membro
-║├────> promover a ADM
-║├─────────────────────
 ║├> ${prefix}demote @adm
-║├> ${prefix}promover @adm
-║├────> rebaixar a membro
-║├─────────────────────
-║├> ${prefix}grup abrir/fechar
-║├> ${prefix}group abrir/fechar
+║├> ${prefix}rebaixar @adm
 ║├> ${prefix}grupo abrir/fechar
-║├────> bem específico ;-;
-║├─────────────────────
-║├> ${prefix}setname seu txt
-║├> ${prefix}mudarnome seu txt
-║├────> troca nome do grupo
-║├─────────────────────
+║├> ${prefix}mudarnome seu txt(muda nome do gp)
+║├> ${prefix}listaadms
+║├> ${prefix}listadmins
+║├> ${prefix}adms
 ║├> ${prefix}setdesc seu txt
 ║├> ${prefix}mudardesc seu txt
-║├────> troca descrição
-║├─────────────────────
 ║├> ${prefix}mudarfoto
-║├────> mudar foto do grupo
-║├─────────────────────
 ║├> ${prefix}marcar
-║├────> marcação
+
+║├> MIDIA/OUTROS
+
 ║├─────────────────────
-║├> ${prefix}marcar2
-║├────> marcação 2
+       FOTO LOFI(única)
+║├>${prefix}lofi
 ║├─────────────────────
-║├> ${prefix}marcar3
-║├────> marcação
-║├─────────────────────
-║├> ᎠϴᏔΝᏞϴᎪᎠ/ᏞᏆΝᏦ
-║├─────────────────────
-║├> ${prefix}tiktokstalk Nick da conta
-║├────> stalk de tiktok
-║├─────────────────────
+       TIK TOK
+║├> ${prefix}tiktokstalk (Nick da conta)
 ║├> ${prefix}fototiktok
-║├────> foto do tiktok
 ║├─────────────────────
+     BAIXA VIDEO DO YOUTUBE
 ║├> ${prefix}ytmp4 link
 ║├> ${prefix}ytvideo link
 ║├> ${prefix}ytbuscar link
 ║├> ${prefix}ytbaixa link
-║├────> baixa vídeo do YouTube
 ║├─────────────────────
-║├>ᎷᏆᎠᏆᎪ/ϴႮͲᎡϴՏ
+    TEMPORARIAMENTE OFF
+║├> ${prefix}sticker|||||||||||||||||
+║├> ${prefix}stiker |||||||||||||||||
+║├> ${prefix}fig    |||||||||||||||||
+║├> ${prefix}figurinha|||||||||||||||
+║├────> fazer fig cm foto/gif|||||
+....................................
 ║├─────────────────────
-║├> ${prefix}sticker
-║├> ${prefix}stiker
-║├> ${prefix}fig
-║├> ${prefix}figurinha
-║├────> fazer fig cm foto/gif
-║├─────────────────────
+      CONVERTE TEXTO EM ÁUDIO
 ║├> ${prefix}tts seu txt
 ║├> ${prefix}gtts seu txt
 ║├> ${prefix}audio seu txt
-║├────> converter txt em áudio
 ║├─────────────────────
+    CONVERTE FIGURINHA EM IMAGEM
 ║├> ${prefix}toimg marca fig
 ║├> ${prefix}converter marca fig
-║├────> converter fig em foto
 ║├─────────────────────
+     PEGA O TEXTO DA FOTO
 ║├> ${prefix}txtdafoto
 ║├> ${prefix}ocr
-║├────> pega txt de foto
 ║├─────────────────────
+      PESQUISA NOME DO ANIME
 ║├> ${prefix}wait marca foto
 ║├> ${prefix}pesquisar marca foto
-║├────> pesquisa por anime de ft
 ║├─────────────────────
-║├> ${prefix}listaadms
-║├> ${prefix}listadmins
-║├> ${prefix}adms
-║├────> lista dos adms
-║├─────────────────────
+    FOTO DE DOG ALEATÓRIO
 ║├> ${prefix}cachorro
 ║├> ${prefix}auau
 ║├> ${prefix}dog
-║├────> dog fofo aleatório
+║├─────────────────────
+      POKEMON ALEATÓRIO
 ║├> ${prefix}pokemon
-║├────> pokemon aleatório
 ║├─────────────────────
+       LOGO COM SEU TEXTO(off)
 ║├> ${prefix}logo3d seu txt
-║├────> logo cm seu texto
-║├─────────────────────
 ║├> ${prefix}logoph seu|txt
-║├────> logo cm seu texto
 ║├─────────────────────
+      PEGA FOTO DO LOCAL
+║├> ${prefix}map 0000-0000(cep) 
+║├> ${prefix}mapa 0000-0000(cep)
+║├─────────────────────
+
+      INFORMAÇÕES DO BOT
 ║├> ${prefix}info
-║├────> informações do bot
-║├─────────────────────
 ║├> ${prefix}listabloq
-║├────> números bloqueados
 ║├─────────────────────
-║├> ${prefix}lofi
-║├────> foto única de lofi
-║├─────────────────────
-║├> ${prefix}map 0000-0000 
-║├> ${prefix} ${prefix}mapa 0000-0000 
-║├────> pega foto do local
-║├─────────────────────
-║├>ᏟϴᎷᎪΝᎠϴՏ Ꭰϴ ᏟᎡᏆᎪᎠϴᎡ
+
+       ᏟϴᎷᎪΝᎠϴՏ Ꭰϴ ᏟᎡᏆᎪᎠϴᎡ
 ║├─────────────────────
 ║├> ${prefix}hidetag
-║├────> esconde tag
-║├─────────────────────
 ║├> ${prefix}clearall
 ║├> ${prefix}limpar
-║├────> limpa zap do bot
-║├─────────────────────
 ║├> ${prefix}bloquear @membro
-║├────> autobloq
-║├─────────────────────
 ║├> ${prefix}desbloquear
-║├────> autodesbloq
-║├─────────────────────
 ║├> ${prefix}kitar
-║├────> bot sai fo grupo
-║├─────────────────────
 ║├> ${prefix}bc
-║├────> [?]
-║├─────────────────────
 ║├> ${prefix}clonar
 ║├> ${prefix}clone
-║├────> clonar tag
+║├> ${prefix}sigla (nova sigla)
+
+             ՏႮᏢϴᎡͲᎬ
 ║├─────────────────────
-║├>  ՏႮᏢϴᎡͲᎬ
-║├─────────────────────
+        BUG REPORT
 ║├> ${prefix}bug
 ║├> ${prefix}reportar
 ║├> ${prefix}reportarbug
-║├────> reportar bug ao criador
-║├─────────────────────
-║├> ${prefix}sigla nova sigla
-║├> ${prefix}setprefix
-║├────> troca sigla de comandos
+
+  se quiser falar com meu dono chame ele nesse numero: wa.me/+5561993845817
 ║└─────────────────────
 ╚══════════════════════
-    ╲    ╱    ●CREITIM●BOT●
-       ╱▔▔▔▔▔╲       
-      ┃┈▇┈┈▇┈┃      
-╭╮┣━━━━━━┫╭╮    
-┃┃┃┈┈┈┈┈┈┃┃┃    
-╰╯┃┈┈┈┈┈┈┃╰╯
-      ╰┓┏━━┓┏╯
-         ╰╯      ╰╯
-┌─────────4.0────────┐
-          sʜɪᴛs_ᴍᴀɴɢᴀ_ʀᴏsᴀ 
-└─────────4.0────────┘
+
+           C R E I T I M  B O T
+
+     ┌─────────😎────────┐
+              sʜɪᴛs_ᴍᴀɴɢᴀ_ʀᴏsᴀ 
+     └─────────😎────────┘
 
 `
 }
